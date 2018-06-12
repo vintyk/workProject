61707b507d1c6688dd20f9ca90e360a909728eb1
vinty1978@gmail.com <djkxtyjr3>
2018-06-12 15:13:30 +0300

    init

--- src/app/components/app/app.module.ts
+++ src/app/components/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { MyComponent } from '../my/my.component';
+import { My2Component } from '../my2/my2.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent,
+    My2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/my/my.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-my',
+  templateUrl: './my.component.html'
+})
+export class MyComponent {
+}
