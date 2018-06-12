fc002a9542052e38e3f5f44a0e211b8d70685bed
vinty1978@gmail.com <djkxtyjr3>
2018-06-12 16:47:57 +0300

    init

--- src/app/components/app/app.module.ts
+++ src/app/components/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyComponent } from '../my/my.component';
-import { My2Component } from '../my2/my2.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent,
-    My2Component
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/my/my.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-my',
-  templateUrl: './my.component.html'
-})
-export class MyComponent {
-}
