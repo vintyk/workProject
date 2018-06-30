253f9735a95071017ba5adb12c2232a0f0fec911
vinty1978@gmail.com <djkxtyjr3>
2018-06-30 16:39:16 +0300

    http-server put delete

--- src/app/components/app/http-server/http-server.component.ts
+++ src/app/components/app/http-server/http-server.component.ts
@@ -1,26 +1,77 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CarsService} from '../../services/cars.service';
-import {Response} from '@angular/http';
+
+interface Car {
+  name: string;
+  color: string;
+  id: number;
+}
 
 @Component({
   selector: 'app-http-server',
   templateUrl: './http-server.component.html',
   styleUrls: ['./http-server.component.css']
 })
-export class HttpServerComponent {
-  cars = [];
+export class HttpServerComponent implements OnInit {
+  cars: Car[] = [];
+  carName = '';
+  color = [
+    'red',
+    'blue',
+    'green',
+    'yellow',
+    'grey'
+  ];
+
+  ngOnInit(): void {
+    this.carsService
+      .getCars()
+      .subscribe((cars: Car[]) => {
+        this.cars = cars;
+      });
+  }
+
+  getRandColor() {
+    const num = Math.round(Math.random() * (this.color.length - 1));
+    return this.color[num];
+  }
+
+  setNewColor(car: Car) {
+    this.carsService.changeColor(car, this.getRandColor())
+      .subscribe((data) => {
+        console.log(data);
+
+      });
+  }
 
   constructor(private carsService: CarsService) {
   }
 
   // это Observeble объект. Он является stream.
-  // не него можно подписаться
+  // на него можно подписаться
   // выполним по завершению стрима callBack функцию
   loadCars() {
-     this.carsService
+    this.carsService
       .getCars()
-      .subscribe((data) => {
-        this.cars = data;
+      .subscribe((cars: Car[]) => {
+        this.cars = cars;
       });
+  }
+
+  addCar(carName: string) {
+    this.carsService
+      .addCar(this.carName)
+      .subscribe((car: Car) => {
+        this.cars.push(car);
+        console.log(car);
+      });
+    this.carName = '';
+  }
+
+  deleteCar(car: Car) {
+    this.carsService.deleteCar(car)
+      .subscribe((data) => {
+        console.log(data);
+      })
   }
 }
--- src/app/components/services/cars.service.ts
+++ src/app/components/services/cars.service.ts
@@ -10,4 +10,22 @@ export class CarsService {
     // возвращает Observeble объект
     return this.http.get('http://localhost:3000/cars');
   }
+
+  addCar(carName: string) {
+    const data = {
+      name: carName,
+      color: 'blue'
+    };
+    return this.http.post('http://localhost:3000/cars', data);
+  }
+
+  changeColor (car: any, color: string) {
+    car.color = color;
+    // put изменение какого-то объекта
+    return this.http.put(`http://localhost:3000/cars/${car.id}`, car);
+  }
+
+  deleteCar (car: any) {
+    return this.http.delete(`http://localhost:3000/cars/${car.id}`);
+  }
 }
